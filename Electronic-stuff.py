d6bb853948d949e9ad34b7467e874d22e9d7f4e6
brozorb <brozorb.yt@gmail.com>
2021-06-22 20:50:37 +0100

    update

--- Electronic-stuff.py
+++ Electronic-stuff.py
@@ -2,3 +2,19 @@ from gpiozero import Led, Button
 from signal import pause
 
 print('the program is starting')
+
+led = LED(17)       # define LED pin according to BCM Numbering
+button = Button(18) # define Button pin according to BCM Numbering
+
+def onButtonPressed():
+	led.on()
+	print("Button is pressed, led turned on >>>")
+
+def onButtonReleased():
+	led.off()
+	print("Button is released, led turned on <<<")
+
+button.when_pressed = onButtonPressed
+button.when_released = onButtonReleased
+
+pause()
